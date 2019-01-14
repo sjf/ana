7b0d65d125ceebb7bc4870b4f0e78eeeba23e9a7
sjf <sarah.fortune@gmail.com>
2019-01-13 23:25:37 -0800

    Use jquery.

--- src/web/static/app.js
+++ src/web/static/app.js
@@ -23,42 +23,46 @@ function pad(num, len) {
   return "0".repeat(len - str.length) + str 
 }
 
-function start_game(){
-  accepted = [];
-  rejected = [];
-  end_time = get_timestamp() + server_remaining_secs;
-  timer = setInterval(update_time, 500); // 0.5 secs
-  document.getElementById("input").disabled = false;
-}
+
 
 function end_game() {
   disable_form();
   clearInterval(timer);
-  var answerElem = document.getElementById("answer");
 }
 
 function update_time() {
-  var remaining = end_time - get_timestamp();
+  var remaining = Math.max(end_time - get_timestamp(), 0);
   var mins = Math.floor(remaining / 60);
   var secs = remaining % 60;
   var txt = mins + ":" + pad(secs, 2);
-  document.getElementById("timer").innerHTML = txt;
-  if (remaining == 0) {
+  $("#timer").html(txt);
+  if (remaining <= 0) {
     end_game()
   }
 }
 
 function disable_form() {
-  var inputElem = document.getElementById("input")
-  inputElem.disabled = true;
-  inputElem.value = "";
+  $("#input").disabled = true;
+  $("#input").val("");
 }
 
+$(document).ready(function() {
+  $("form").on("submit", handle_form);
+
+  accepted = [];
+  rejected = [];
+  end_time = get_timestamp() + server_remaining_secs;
+  timer = setInterval(update_time, 500); // 0.5 secs
+  $("#input").disable = false;
+});
+
+
 function handle_form() {
+  event.preventDefault();
   console.log('handle');
-  var input = document.getElementById("input");
-  var txt = input.value;
-  input.value = "";
+
+  var txt = $("input").val();
+  $("input").val("")
   rejected = [];
   var words = txt.split(/, /);
   words.forEach(function (word) {
@@ -71,13 +75,9 @@ function handle_form() {
     }
   });
 
-  var acceptedElem = document.getElementById("accepted");
-  var rejectedElem = document.getElementById("rejected");
-  acceptedElem.innerHTML = accepted.join(", "); // not sanitised
-  rejectedElem.innerHTML = rejected.join(", "); // not sanitised
-
-  var scoreElem = document.getElementById("score_value");
-  scoreElem.innerHTML = calculate_score();
+  $("#accepted").html(accepted.join(", ")); // not sanitised
+  $("#rejected").html(rejected.join(", ")); // not sanitised
+  $("#score_value").html(calculate_score()); 
 }
 
 function calculate_score() {
